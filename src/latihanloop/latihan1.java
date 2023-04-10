d1d5740e3a6a4cd4840e5ffbec2879362ac2f6ff
reyy2212 <reyhanhs22@gmail.com>
2023-04-10 11:33:27 +0700

    Tugas 4 Segitiga terbalik

---
+++ src/latihanloop/latihan1.java
@@ -0,0 +1,35 @@
+package latihanloop;
+
+import java.io.IOException;
+
+public class latihan1 {
+    public static void main(String[] args) throws IOException {
+        //mencetak angka 1-10
+//        for (int i = 1; i <= 10; i++) {
+//            System.out.println(i);
+//        }
+//        //mencetak kata i love you sebanyak 100x
+//        for (int i = 0; 1 < 101 ; i++) {
+//            System.out.println("i love you");
+//        }
+
+        //mencetak jumlahan angka dari 1-10
+//        int n = 11;
+//        int sum = 0;
+//        for (int i = 0; i < n; i++) {
+//            sum += i;
+//        }
+//        System.out.println("hasil " + sum);
+
+
+        //coba penjumlahan 10+9+8+7+6+5+4+3+2+1
+//        int sum = 0;
+//        for (int i = 10; i >= 1; i--) {
+//            sum += i;
+//        }
+//        System.out.println("10+9+8+7+6+5+4+3+2+1 = " + sum);
+
+
+
+    }
+}
